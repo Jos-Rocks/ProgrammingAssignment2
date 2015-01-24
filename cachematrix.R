fc12f18e544f91828c266559264c33e179d55a27
Jos <jos.n.dent@gmail.com>
2015-01-24 14:30:51 +0000

    Second part completed but not tested

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,17 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## Tried to make naming a bit more readable, for example, I have renamed 'x' to 'storedMatrix'
 
-## Write a short comment describing this function
-## Tried to make naming a bit more intuitive, so I have renamed 'x' to 'storedMatrix'
 
+## makeCacheMatrix makes a special 'matrix' that is capable of caching its inverse
 makeCacheMatrix <- function(storedMatrix = matrix()) {
   storedInverse <- NULL
   getMatrix <- function() {
     storedMatrix
   }
-  setMatrix <- function(newMatrix) {
+  setMatrix <- function(newMatrix) { 
+    ## If the stored matrix changes, this will change the cached inverse to NULL,
+    ## so we won't accidentally use the stale inverse data
     storedInverse <<- NULL
     storedMatrix <<- newMatrix
   }
@@ -28,6 +30,15 @@ makeCacheMatrix <- function(storedMatrix = matrix()) {
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cachingMatrix, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  solved <- cachingMaxtrix$getInverse()
+  if(!is.null(solved)) {
+    message("getting cached data")
+    return(solved)
+  }
+  data <- cachingMatrix$getMatrix()
+  solved <- solve(data, ...)
+  cachingMatrix$setInverse(m)
+  solved
 }
